ee5453ef2b3ccb58c8044ee315e3ea1b4e28605b
Shen <772639270@qq.com>
2023-03-14 00:12:33 +0800

    线程池工作仿函数

---
+++ thread_worker.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 77263 on 2023/3/14.
+//
+
+#include "thread_worker.h"
---
+++ thread_worker.h
@@ -0,0 +1,26 @@
+//
+// Created by 77263 on 2023/3/14.
+//
+
+#ifndef THREADPOOL_THREAD_WORKER_H
+#define THREADPOOL_THREAD_WORKER_H
+
+
+class ThreadWorker
+{
+public:
+    void operator()()
+    {
+        while(1)
+        {
+
+        }
+
+    }
+private:
+    // ThreadPool* threadPool;
+    int id;
+};
+
+
+#endif //THREADPOOL_THREAD_WORKER_H
